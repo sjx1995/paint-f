00711a029c06893d8d4e3fc8d5574017e457d3f9
sunly <sunjingxiang1995@gmail.com>
2023-08-05 06:13:49 +0000

    init

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<!--
+ * @Description: 
+ * @Author: Sunly
+ * @Date: 2023-08-05 04:07:11
+-->
+<script setup lang="ts"></script>
+
+<template>
+  <div id="test">
+    <v-btn prepend-icon="$vuetify" variant="outlined"> Button </v-btn>
+  </div>
+</template>
+
+<style scoped lang="scss">
+#test {
+  .v-btn {
+    color: red;
+  }
+}
+</style>
